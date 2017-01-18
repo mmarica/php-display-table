81af30a30767bbf106010e67ee6e9e01cfe69c34
Mihai Marica <mihai.marica82@yahoo.com>
2017-01-18 23:03:54 +0200

    fixed method call typo in tests

--- tests/src/DisplayTable/Output/TextOutputTest.php
+++ tests/src/DisplayTable/Output/TextOutputTest.php
@@ -67,7 +67,7 @@ class TextOutputTest extends AbstractTest
     {
         $output = new TextOutput($this->_oneHeaderMultipleRows);
         $result = $output->noPadding()->generate();
-        $expected = $this->_loadTxtResource(__METHOD__);
+        $expected = $this->_loadResource(__METHOD__);
         $this->assertSame($expected, $result);
     }
 
@@ -76,7 +76,7 @@ class TextOutputTest extends AbstractTest
         $output = new TextOutput($this->_oneHeaderMultipleRows);
         $result = $output->hPadding(2)->vPadding(1)->generate();
 
-        $expected = $this->_loadTxtResource(__METHOD__);
+        $expected = $this->_loadResource(__METHOD__);
         $this->assertSame($expected, $result);
 
         $this->assertSame($output->getHPadding(), 2);
@@ -91,378 +91,378 @@ class TextOutputTest extends AbstractTest
 
     public function test_BubbleBorder_NoHeadersMultipleRows()
     {
-        $expected = $this->_loadTxtResource(__METHOD__);
+        $expected = $this->_loadResource(__METHOD__);
         $output = new TextOutput($this->_noHeadersMultipleRows);
         $this->assertSame($expected, $output->bubbleBorder()->generate());
     }
 
     public function test_BubbleBorder_NoHeadersOneRow()
     {
-        $expected = $this->_loadTxtResource(__METHOD__);
+        $expected = $this->_loadResource(__METHOD__);
         $output = new TextOutput($this->_noHeadersOneRow);
         $this->assertSame($expected, $output->bubbleBorder()->generate());
     }
 
     public function test_BubbleBorder_OneHeaderMultipleRows()
     {
-        $expected = $this->_loadTxtResource(__METHOD__);
+        $expected = $this->_loadResource(__METHOD__);
         $output = new TextOutput($this->_oneHeaderMultipleRows);
         $this->assertSame($expected, $output->bubbleBorder()->generate());
     }
 
     public function test_BubbleBorder_OneHeaderNoRows()
     {
-        $expected = $this->_loadTxtResource(__METHOD__);
+        $expected = $this->_loadResource(__METHOD__);
         $output = new TextOutput($this->_oneHeaderNoRows);
         $this->assertSame($expected, $output->bubbleBorder()->generate());
     }
 
     public function test_BubbleBorder_MultipleHeadersNoRows()
     {
-        $expected = $this->_loadTxtResource(__METHOD__);
+        $expected = $this->_loadResource(__METHOD__);
         $output = new TextOutput($this->_multipleHeadersNoRows);
         $this->assertSame($expected, $output->bubbleBorder()->generate());
     }
 
     public function test_BubbleBorder_MultipleHeadersMultipleRows()
     {
-        $expected = $this->_loadTxtResource(__METHOD__);
+        $expected = $this->_loadResource(__METHOD__);
         $output = new TextOutput($this->_multipleHeadersMultipleRows);
         $this->assertSame($expected, $output->bubbleBorder()->generate());
     }
 
     public function test_CompactBorder_NoHeadersMultipleRows()
     {
-        $expected = $this->_loadTxtResource(__METHOD__);
+        $expected = $this->_loadResource(__METHOD__);
         $output = new TextOutput($this->_noHeadersMultipleRows);
         $this->assertSame($expected, $output->compactBorder()->generate());
     }
 
     public function test_CompactBorder_NoHeadersOneRow()
     {
-        $expected = $this->_loadTxtResource(__METHOD__);
+        $expected = $this->_loadResource(__METHOD__);
         $output = new TextOutput($this->_noHeadersOneRow);
         $this->assertSame($expected, $output->compactBorder()->generate());
     }
 
     public function test_CompactBorder_OneHeaderMultipleRows()
     {
-        $expected = $this->_loadTxtResource(__METHOD__);
+        $expected = $this->_loadResource(__METHOD__);
         $output = new TextOutput($this->_oneHeaderMultipleRows);
         $this->assertSame($expected, $output->compactBorder()->generate());
     }
 
     public function test_CompactBorder_OneHeaderNoRows()
     {
-        $expected = $this->_loadTxtResource(__METHOD__);
+        $expected = $this->_loadResource(__METHOD__);
         $output = new TextOutput($this->_oneHeaderNoRows);
         $this->assertSame($expected, $output->compactBorder()->generate());
     }
 
     public function test_CompactBorder_MultipleHeadersNoRows()
     {
-        $expected = $this->_loadTxtResource(__METHOD__);
+        $expected = $this->_loadResource(__METHOD__);
         $output = new TextOutput($this->_multipleHeadersNoRows);
         $this->assertSame($expected, $output->compactBorder()->generate());
     }
 
     public function test_CompactBorder_MultipleHeadersMultipleRows()
     {
-        $expected = $this->_loadTxtResource(__METHOD__);
+        $expected = $this->_loadResource(__METHOD__);
         $output = new TextOutput($this->_multipleHeadersMultipleRows);
         $this->assertSame($expected, $output->compactBorder()->generate());
     }
 
     public function test_DifferentiatedBorder_NoHeadersMultipleRows()
     {
-        $expected = $this->_loadTxtResource(__METHOD__);
+        $expected = $this->_loadResource(__METHOD__);
         $output = new TextOutput($this->_noHeadersMultipleRows);
         $this->assertSame($expected, $output->differentiatedBorder()->generate());
     }
 
     public function test_DifferentiatedBorder_NoHeadersOneRow()
     {
-        $expected = $this->_loadTxtResource(__METHOD__);
+        $expected = $this->_loadResource(__METHOD__);
         $output = new TextOutput($this->_noHeadersOneRow);
         $this->assertSame($expected, $output->differentiatedBorder()->generate());
     }
 
     public function test_DifferentiatedBorder_OneHeaderMultipleRows()
     {
-        $expected = $this->_loadTxtResource(__METHOD__);
+        $expected = $this->_loadResource(__METHOD__);
         $output = new TextOutput($this->_oneHeaderMultipleRows);
         $this->assertSame($expected, $output->differentiatedBorder()->generate());
     }
 
     public function test_DifferentiatedBorder_OneHeaderNoRows()
     {
-        $expected = $this->_loadTxtResource(__METHOD__);
+        $expected = $this->_loadResource(__METHOD__);
         $output = new TextOutput($this->_oneHeaderNoRows);
         $this->assertSame($expected, $output->differentiatedBorder()->generate());
     }
 
     public function test_DifferentiatedBorder_MultipleHeadersNoRows()
     {
-        $expected = $this->_loadTxtResource(__METHOD__);
+        $expected = $this->_loadResource(__METHOD__);
         $output = new TextOutput($this->_multipleHeadersNoRows);
         $this->assertSame($expected, $output->differentiatedBorder()->generate());
     }
 
     public function test_DifferentiatedBorder_MultipleHeadersMultipleRows()
     {
-        $expected = $this->_loadTxtResource(__METHOD__);
+        $expected = $this->_loadResource(__METHOD__);
         $output = new TextOutput($this->_multipleHeadersMultipleRows);
         $this->assertSame($expected, $output->differentiatedBorder()->generate());
     }
 
     public function test_DottedBorder_NoHeadersMultipleRows()
     {
-        $expected = $this->_loadTxtResource(__METHOD__);
+        $expected = $this->_loadResource(__METHOD__);
         $output = new TextOutput($this->_noHeadersMultipleRows);
         $this->assertSame($expected, $output->dottedBorder()->generate());
     }
 
     public function test_DottedBorder_NoHeadersOneRow()
     {
-        $expected = $this->_loadTxtResource(__METHOD__);
+        $expected = $this->_loadResource(__METHOD__);
         $output = new TextOutput($this->_noHeadersOneRow);
         $this->assertSame($expected, $output->dottedBorder()->generate());
     }
 
     public function test_DottedBorder_OneHeaderMultipleRows()
     {
-        $expected = $this->_loadTxtResource(__METHOD__);
+        $expected = $this->_loadResource(__METHOD__);
         $output = new TextOutput($this->_oneHeaderMultipleRows);
         $this->assertSame($expected, $output->dottedBorder()->generate());
     }
 
     public function test_DottedBorder_OneHeaderNoRows()
     {
-        $expected = $this->_loadTxtResource(__METHOD__);
+        $expected = $this->_loadResource(__METHOD__);
         $output = new TextOutput($this->_oneHeaderNoRows);
         $this->assertSame($expected, $output->dottedBorder()->generate());
     }
 
     public function test_DottedBorder_MultipleHeadersNoRows()
     {
-        $expected = $this->_loadTxtResource(__METHOD__);
+        $expected = $this->_loadResource(__METHOD__);
         $output = new TextOutput($this->_multipleHeadersNoRows);
         $this->assertSame($expected, $output->dottedBorder()->generate());
     }
 
     public function test_DottedBorder_MultipleHeadersMultipleRows()
     {
-        $expected = $this->_loadTxtResource(__METHOD__);
+        $expected = $this->_loadResource(__METHOD__);
         $output = new TextOutput($this->_multipleHeadersMultipleRows);
         $this->assertSame($expected, $output->dottedBorder()->generate());
     }
 
     public function test_GirderBorder_NoHeadersMultipleRows()
     {
-        $expected = $this->_loadTxtResource(__METHOD__);
+        $expected = $this->_loadResource(__METHOD__);
         $output = new TextOutput($this->_noHeadersMultipleRows);
         $this->assertSame($expected, $output->girderBorder()->generate());
     }
 
     public function test_GirderBorder_NoHeadersOneRow()
     {
-        $expected = $this->_loadTxtResource(__METHOD__);
+        $expected = $this->_loadResource(__METHOD__);
         $output = new TextOutput($this->_noHeadersOneRow);
         $this->assertSame($expected, $output->girderBorder()->generate());
     }
 
     public function test_GirderBorder_OneHeaderMultipleRows()
     {
-        $expected = $this->_loadTxtResource(__METHOD__);
+        $expected = $this->_loadResource(__METHOD__);
         $output = new TextOutput($this->_oneHeaderMultipleRows);
         $this->assertSame($expected, $output->girderBorder()->generate());
     }
 
     public function test_GirderBorder_OneHeaderNoRows()
     {
-        $expected = $this->_loadTxtResource(__METHOD__);
+        $expected = $this->_loadResource(__METHOD__);
         $output = new TextOutput($this->_oneHeaderNoRows);
         $this->assertSame($expected, $output->girderBorder()->generate());
     }
 
     public function test_GirderBorder_MultipleHeadersNoRows()
     {
-        $expected = $this->_loadTxtResource(__METHOD__);
+        $expected = $this->_loadResource(__METHOD__);
         $output = new TextOutput($this->_multipleHeadersNoRows);
         $this->assertSame($expected, $output->girderBorder()->generate());
     }
 
     public function test_GirderBorder_MultipleHeadersMultipleRows()
     {
-        $expected = $this->_loadTxtResource(__METHOD__);
+        $expected = $this->_loadResource(__METHOD__);
         $output = new TextOutput($this->_multipleHeadersMultipleRows);
         $this->assertSame($expected, $output->girderBorder()->generate());
     }
 
     public function test_GithubBorder_NoHeadersMultipleRows()
     {
-        $expected = $this->_loadTxtResource(__METHOD__);
+        $expected = $this->_loadResource(__METHOD__);
         $output = new TextOutput($this->_noHeadersMultipleRows);
         $this->assertSame($expected, $output->githubBorder()->generate());
     }
 
     public function test_GithubBorder_NoHeadersOneRow()
     {
-        $expected = $this->_loadTxtResource(__METHOD__);
+        $expected = $this->_loadResource(__METHOD__);
         $output = new TextOutput($this->_noHeadersOneRow);
         $this->assertSame($expected, $output->githubBorder()->generate());
     }
 
     public function test_GithubBorder_OneHeaderMultipleRows()
     {
-        $expected = $this->_loadTxtResource(__METHOD__);
+        $expected = $this->_loadResource(__METHOD__);
         $output = new TextOutput($this->_oneHeaderMultipleRows);
         $this->assertSame($expected, $output->githubBorder()->generate());
     }
 
     public function test_GithubBorder_OneHeaderNoRows()
     {
-        $expected = $this->_loadTxtResource(__METHOD__);
+        $expected = $this->_loadResource(__METHOD__);
         $output = new TextOutput($this->_oneHeaderNoRows);
         $this->assertSame($expected, $output->githubBorder()->generate());
     }
 
     public function test_GithubBorder_MultipleHeadersNoRows()
     {
-        $expected = $this->_loadTxtResource(__METHOD__);
+        $expected = $this->_loadResource(__METHOD__);
         $output = new TextOutput($this->_multipleHeadersNoRows);
         $this->assertSame($expected, $output->githubBorder()->generate());
     }
 
     public function test_GithubBorder_MultipleHeadersMultipleRows()
     {
-        $expected = $this->_loadTxtResource(__METHOD__);
+        $expected = $this->_loadResource(__METHOD__);
         $output = new TextOutput($this->_multipleHeadersMultipleRows);
         $this->assertSame($expected, $output->githubBorder()->generate());
     }
 
     public function test_MysqlBorder_NoHeadersMultipleRows()
     {
-        $expected = $this->_loadTxtResource(__METHOD__);
+        $expected = $this->_loadResource(__METHOD__);
         $output = new TextOutput($this->_noHeadersMultipleRows);
         $this->assertSame($expected, $output->mysqlBorder()->generate());
     }
 
     public function test_MysqlBorder_NoHeadersOneRow()
     {
-        $expected = $this->_loadTxtResource(__METHOD__);
+        $expected = $this->_loadResource(__METHOD__);
         $output = new TextOutput($this->_noHeadersOneRow);
         $this->assertSame($expected, $output->mysqlBorder()->generate());
     }
 
     public function test_MysqlBorder_OneHeaderMultipleRows()
     {
-        $expected = $this->_loadTxtResource(__METHOD__);
+        $expected = $this->_loadResource(__METHOD__);
         $output = new TextOutput($this->_oneHeaderMultipleRows);
         $this->assertSame($expected, $output->mysqlBorder()->generate());
     }
 
     public function test_MysqlBorder_OneHeaderNoRows()
     {
-        $expected = $this->_loadTxtResource(__METHOD__);
+        $expected = $this->_loadResource(__METHOD__);
         $output = new TextOutput($this->_oneHeaderNoRows);
         $this->assertSame($expected, $output->mysqlBorder()->generate());
     }
 
     public function test_MysqlBorder_MultipleHeadersNoRows()
     {
-        $expected = $this->_loadTxtResource(__METHOD__);
+        $expected = $this->_loadResource(__METHOD__);
         $output = new TextOutput($this->_multipleHeadersNoRows);
         $this->assertSame($expected, $output->mysqlBorder()->generate());
     }
 
     public function test_MysqlBorder_MultipleHeadersMultipleRows()
     {
-        $expected = $this->_loadTxtResource(__METHOD__);
+        $expected = $this->_loadResource(__METHOD__);
         $output = new TextOutput($this->_multipleHeadersMultipleRows);
         $this->assertSame($expected, $output->mysqlBorder()->generate());
     }
 
     public function test_NoBorder_NoHeadersMultipleRows()
     {
-        $expected = $this->_loadTxtResource(__METHOD__);
+        $expected = $this->_loadResource(__METHOD__);
         $output = new TextOutput($this->_noHeadersMultipleRows);
         $this->assertSame($expected, $output->noBorder()->generate());
     }
 
     public function test_NoBorder_NoHeadersOneRow()
     {
-        $expected = $this->_loadTxtResource(__METHOD__);
+        $expected = $this->_loadResource(__METHOD__);
         $output = new TextOutput($this->_noHeadersOneRow);
         $this->assertSame($expected, $output->noBorder()->generate());
     }
 
     public function test_NoBorder_OneHeaderMultipleRows()
     {
-        $expected = $this->_loadTxtResource(__METHOD__);
+        $expected = $this->_loadResource(__METHOD__);
         $output = new TextOutput($this->_oneHeaderMultipleRows);
         $this->assertSame($expected, $output->noBorder()->generate());
     }
 
     public function test_NoBorder_OneHeaderNoRows()
     {
-        $expected = $this->_loadTxtResource(__METHOD__);
+        $expected = $this->_loadResource(__METHOD__);
         $output = new TextOutput($this->_oneHeaderNoRows);
         $this->assertSame($expected, $output->noBorder()->generate());
     }
 
     public function test_NoBorder_MultipleHeadersNoRows()
     {
-        $expected = $this->_loadTxtResource(__METHOD__);
+        $expected = $this->_loadResource(__METHOD__);
         $output = new TextOutput($this->_multipleHeadersNoRows);
         $this->assertSame($expected, $output->noBorder()->generate());
     }
 
     public function test_NoBorder_MultipleHeadersMultipleRows()
     {
-        $expected = $this->_loadTxtResource(__METHOD__);
+        $expected = $this->_loadResource(__METHOD__);
         $output = new TextOutput($this->_multipleHeadersMultipleRows);
         $this->assertSame($expected, $output->noBorder()->generate());
     }
 
     public function test_RoundedBorder_NoHeadersMultipleRows()
     {
-        $expected = $this->_loadTxtResource(__METHOD__);
+        $expected = $this->_loadResource(__METHOD__);
         $output = new TextOutput($this->_noHeadersMultipleRows);
         $this->assertSame($expected, $output->roundedBorder()->generate());
     }
 
     public function test_RoundedBorder_NoHeadersOneRow()
     {
-        $expected = $this->_loadTxtResource(__METHOD__);
+        $expected = $this->_loadResource(__METHOD__);
         $output = new TextOutput($this->_noHeadersOneRow);
         $this->assertSame($expected, $output->roundedBorder()->generate());
     }
 
     public function test_RoundedBorder_OneHeaderMultipleRows()
     {
-        $expected = $this->_loadTxtResource(__METHOD__);
+        $expected = $this->_loadResource(__METHOD__);
         $output = new TextOutput($this->_oneHeaderMultipleRows);
         $this->assertSame($expected, $output->roundedBorder()->generate());
     }
 
     public function test_RoundedBorder_OneHeaderNoRows()
     {
-        $expected = $this->_loadTxtResource(__METHOD__);
+        $expected = $this->_loadResource(__METHOD__);
         $output = new TextOutput($this->_oneHeaderNoRows);
         $this->assertSame($expected, $output->roundedBorder()->generate());
     }
 
     public function test_RoundedBorder_MultipleHeadersNoRows()
     {
-        $expected = $this->_loadTxtResource(__METHOD__);
+        $expected = $this->_loadResource(__METHOD__);
         $output = new TextOutput($this->_multipleHeadersNoRows);
         $this->assertSame($expected, $output->roundedBorder()->generate());
     }
 
     public function test_RoundedBorder_MultipleHeadersMultipleRows()
     {
-        $expected = $this->_loadTxtResource(__METHOD__);
+        $expected = $this->_loadResource(__METHOD__);
         $output = new TextOutput($this->_multipleHeadersMultipleRows);
         $this->assertSame($expected, $output->roundedBorder()->generate());
     }
